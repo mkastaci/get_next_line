5ee4398ad2d59f32ad54b7991bf84e2026799de6
mikail kastaci <mikail@MacBook-Pro-de-mikail.local>
2018-11-26 00:10:31 +0100

    envoie ecole

--- get_next_line.c
+++ get_next_line.c
@@ -17,20 +17,71 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-int get_next_line(const int fd, char **line)
+char	*lirefichier(int fd, char *str)
 {
+	char	buff[BUFF_SIZE + 1];
+	int		ret;
 
+	while ((ret = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[ret] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	return (str);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char *str;
+	int i;
+	int g;
+
+	g = 0;
+	i = 0;
+	if (fd == -1 || line == NULL)
+		return (-1);
+	if (str == NULL)
+	{
+		if (!(str = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+			return (-1);
+	}
+	str = lirefichier(fd, str);
+	if (str[i])
+	{
+		while (str[i] != '\n' && str[i] != '\n')
+			i++;
+		if (i == 0)
+			(*line) = ft_strdup("");
+		else
+		{
+			(*line) = ft_strsub(str, 0, i);
+			str = &str[i + 1];
+		}
+		printf("%s \n", line[g]);
+		g++;
+		return (1);
+	}
+	else
+		(*line) = ft_strdup("");
+	return (0);
 }
 
 
 int main(int argc, char **argv)
 {
-	int fd;
+	int		fd;
+	char	*line;
 
-	fd = open(argv[1], O_RDONLY | O_APPEND, S_IRUSR);
+	line = NULL;
 	if (argc > 1)
 	{
-		printf("%d \n", fd);
+		fd = open(argv[1], O_RDONLY);
+		while (get_next_line(fd, &line) > 0)
+		{
+			free(line);
+		}
 	}
-return(0);
+	else
+		write(2, "il manque largument\n", 19);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -11,8 +11,10 @@
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#include "libft/libft.h"
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
 # define BUFF_SIZE 32
 
 int		get_next_line(const int fd, char **line);
